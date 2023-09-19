c74a6ad397eea70bdaa88949a637bbd8ae6c85d1
Meowmaster <ashenoy64@gmail.com>
2023-09-19 11:39:52 +0530

    Added project modal

--- app/page.js
+++ app/page.js
@@ -2,10 +2,13 @@ import Search from "@/components/Search"
 import MusicPlayer from "@/components/MusicPlayer"
 import Playlist from "@/components/Playlist"
 import RecentlyPlayed from "@/components/RecentlyPlayed"
+import Modal from "@/components/ModalViewer"
+
 
 export default function Home() {
   return (
     <main>
+      <Modal/>
       <Search/>
       <RecentlyPlayed/>
       <Playlist/>
