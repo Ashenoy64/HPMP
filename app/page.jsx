3f4cc2b90f9aba9488d3412f9dcd3890c6fe7ce1
Meowmaster <ashenoy64@gmail.com>
2023-12-30 12:40:20 +0530

    Cleaning 4

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,11 @@ import React, { useEffect } from "react";
 import { useRef, useState } from "react";
 
 import { auth } from "@/lib/firebase";
-import { sendSignInLinkToEmail,isSignInWithEmailLink,onAuthStateChanged } from "firebase/auth";
+import { sendSignInLinkToEmail,onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/navigation";
+
+
+
 
 export default function landing() {
   
